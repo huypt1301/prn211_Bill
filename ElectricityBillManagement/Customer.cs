1515f85cb96fe5489677a329179face74f5c9a48
huypt1301 <huyptse150477@fpt.edu.vn>
2022-07-11 21:04:27 +0700

    Ca Nhan

---
+++ ElectricityBillManagement/Customer.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityBillManagement
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public double Bill { get; set; }
+        public Customer()
+        {
+        }
+
+        public Customer(int id, string name, string address, int quantity, int unitprice, double bill)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Quantity = quantity;
+            UnitPrice = unitprice;
+            Bill = bill;
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id} || Name = {Name} || Address = {Address} || Quantity(KW) = {Quantity} || UnitPrice = {UnitPrice} || Bill = {Bill}";
+        }
+    }
+    public class DomesticCustomers : Customer
+    {
+        public string ObjectCustomer { get; set; }
+        public int Limit { get; set; }
+
+        public DomesticCustomers()
+        {
+        }
+
+        public DomesticCustomers(int id, string name, string address, string objectCustomer, int quantity, int unitprice, int limit, double bill) :base (id,name, address,quantity,unitprice, bill)
+        {
+            ObjectCustomer = objectCustomer;
+            Limit = limit;
+        }
+        public DomesticCustomers Intput()
+        {
+            Console.WriteLine("Enter ID: ");
+            Id = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Emter Address: ");
+            Address = Console.ReadLine();
+            Console.WriteLine("Enter Kind Cus: ");
+            ObjectCustomer = Console.ReadLine();
+            Console.WriteLine("Enter quantity (kw) : ");
+            Quantity = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Price: ");
+            UnitPrice = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Limit: ");
+            Limit = Int32.Parse(Console.ReadLine());
+            Bill = this.ResultBill(Quantity, Limit, UnitPrice);
+            return new DomesticCustomers(Id, Name, Address, ObjectCustomer, Quantity, UnitPrice, Limit, Bill);
+        }
+
+        public double ResultBill(int quantity, int limit, int unitPrice)
+        {
+            double bill;
+            if(quantity <= limit)
+            {
+                bill = quantity * unitPrice;
+            }
+            else
+            {
+                bill = quantity * unitPrice + (quantity - limit) * unitPrice * 2.5;
+            }
+            return bill;
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id} || Name = {Name} || Address = {Address} || ObjectCustomer = {ObjectCustomer} || Quantity(KW) = {Quantity} || UnitPrice = {UnitPrice} || Limit = {Limit} || Bill = {Bill}";
+        }
+    }
+
+    public class ForeignCustomers : Customer
+    {
+        public string Nationality { get; set; }
+
+        public ForeignCustomers()
+        {
+        }
+
+        public ForeignCustomers(int id, string name, string address, string nationality,  int quantity, int unitprice, double bill) :base (id, name, address, quantity, unitprice, bill)
+        {
+            Nationality = nationality;
+        }
+        public ForeignCustomers Intput()
+        {
+            Console.WriteLine("Enter ID: ");
+            Id = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Emter Address: ");
+            Address = Console.ReadLine();
+            Console.WriteLine("Enter the country: ");
+            Nationality = Console.ReadLine();
+            Console.WriteLine("Enter quantity (kw): ");
+            Quantity = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Price: ");
+            UnitPrice = Int32.Parse(Console.ReadLine());
+            Bill = Quantity * UnitPrice;
+            return new  ForeignCustomers(Id, Name, Address, Nationality, Quantity, UnitPrice, Bill);
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id} || Name = {Name} || Address = {Address} || Nationallity = {Nationality} || Quantity(KW) = {Quantity} || UnitPrice = {UnitPrice} || Bill = {Bill}";
+        }
+    }
+}
---
+++ ElectricityBillManagement/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElectricityBillManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DomesticCustomers> list1 = new List<DomesticCustomers>();
+            List<ForeignCustomers> list2 = new List<ForeignCustomers>();
+            DomesticCustomers dc = new DomesticCustomers();
+            ForeignCustomers fc = new ForeignCustomers();
+            int select;
+            do
+            {
+                Console.WriteLine("================================");
+                Console.WriteLine("1.List Customer ");
+                Console.WriteLine("2.total consumption");
+                Console.WriteLine("3.average amount of foreign visitors");
+                Console.WriteLine("Other - Out");
+                Console.WriteLine("Other Choice");
+                select = Int32.Parse(Console.ReadLine());
+                switch (select)
+                {
+                    case 1:
+                        int number = 0;
+                        do
+                        {
+                            Console.WriteLine("========================");
+                            Console.WriteLine("1.Khach hang trong nuoc");
+                            Console.WriteLine("2.Khach hang ngoai nuoc");
+                            Console.WriteLine("Other - Out");
+                            Console.WriteLine("Lua chon:");
+                            number = Int32.Parse(Console.ReadLine());
+                            if (number == 1)
+                            {
+                                DomesticCustomers dcx = dc.Intput();
+                                list1.Add(dcx);
+                                Console.WriteLine(dc);
+                                Console.WriteLine("Success!");
+                            }
+                            else if(number == 2)
+                            {
+                                ForeignCustomers fcx = fc.Intput();
+                                list2.Add(fcx);
+                                Console.WriteLine(fc);
+                                Console.WriteLine("Success!");
+                            }
+                            else
+                            {
+                                number = 0;
+                            }
+                        } while (number <=2 && number >0);
+                        break;
+                    case 2:
+                        int sum1 =0, sum2 = 0;
+                        foreach(DomesticCustomers dcl in list1)
+                        {
+                            sum1 += dcl.Quantity; 
+                        }
+                        foreach (ForeignCustomers fcl in list2)
+                        {
+                            sum2 += fcl.Quantity;
+                        }
+                        Console.WriteLine("Total KW:" + (sum1 + sum2));
+                        Console.WriteLine("Khach hang trong nuoc: " + sum1 + " KW");
+                        Console.WriteLine("Khach hang ngoai nuoc: " + sum2 + " KW");
+                        break;
+                    case 3:
+                        double sumBill = 0;
+                        int count = 0;
+                        foreach (ForeignCustomers fcl in list2)
+                        {
+                            sumBill += fcl.Bill;
+                            count++;
+                        }
+                        double tbBill = sumBill / count;
+                        Console.WriteLine("average amount of foreign visitors is: " + tbBill);
+                        break;
+                }
+            } while (select <= 3 && select > 0);
+
+
+        }
+    }
+    
+}
